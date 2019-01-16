a76e8fe3a992ea0957d4666a81f8e6809a8fe51c
jbrock1981 <jbrock1981@gmail.com>
2019-01-15 21:40:48 -0600

    completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const bcrypt = require('bcryptjs');
 const db = require('./database/dbConfig.js');
 
 const server = express();
@@ -11,6 +11,45 @@ server.use(cors());
 server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
+
+server.post('/api/register', (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password);
+  db('users')
+    .insert(user)
+    .then(id => {
+      res
+        .status(201)
+        .json({id: ids[0]});
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send(err);
+    })
+});
+
+server.post('/api/login', (req, res) => {
+  const user = req.body;
+
+  db('users')
+    .where('username', user.username)
+    .then(users => {
+      if (users.length && bcrypt.compareSync(bodyUser.password, users[0].password)) {
+        res
+          .json({ info: "correct" })
+      } else {
+        res
+          .status(404)
+          .json({err: "invalis username or password"});
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send(err);
+    })
+})
 
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
